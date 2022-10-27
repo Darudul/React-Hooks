33248abbf222bf4792fb3f840bddf2716160c9b6
Md Darudul Islam <darudulislam@gmail.com>
2022-10-27 18:27:24 +0800

    useEffect with cleanup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import './App.css';
-import HookCounterOne from './components/Hook_useEffect/HookCounterOne';
-import HookMouse from './components/Hook_useEffect/HookMouse';
+// import './App.css';
+// import HookCounterOne from './components/Hook_useEffect/HookCounterOne';
+// import HookMouse from './components/Hook_useEffect/HookMouse';
+import MouseContainer from './components/Hook_useEffect/MouseContainer';
 
 function App() {
   return (
     <div className="App">
       {/* <HookCounterOne /> */}
-      <HookMouse />
+      {/* <HookMouse /> */}
+      <MouseContainer />
+
 
     </div>
   );
