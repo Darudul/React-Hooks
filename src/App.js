faa6d2494496e3fd4513d36faf46279efefee831
Md Darudul Islam <darudulislam@gmail.com>
2022-10-27 18:09:44 +0800

    run effects only once

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import HookCounterOne from './components/Hook_useEffect/HookCounterOne';
+import HookMouse from './components/Hook_useEffect/HookMouse';
 
 function App() {
   return (
     <div className="App">
-      <HookCounterOne />
+      {/* <HookCounterOne /> */}
+      <HookMouse />
 
     </div>
   );
